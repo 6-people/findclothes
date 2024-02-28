924cd487909bf1cafac987670b557f3c2c132af0
chy0503 <202015006@kyonggi.ac.kr>
2024-02-28 21:36:16 +0900

    entity : user role, provider 수정

--- src/main/java/com/people/findclothes/domain/User.java
+++ src/main/java/com/people/findclothes/domain/User.java
@@ -1,6 +1,6 @@
 package com.people.findclothes.domain;
 
-import com.people.findclothes.domain.constant.UserStatus;
+import com.people.findclothes.domain.constant.UserRole;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
@@ -33,8 +33,12 @@ public class User {
     @Column(nullable = false, updatable = false)
     private String name;
 
-    @Column(nullable = false, updatable = false)
-    private UserStatus userStatus;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole userRole;
+
+    @Column(updatable = false)
+    private String provider;
 
     @CreatedDate
     @Column(nullable = false, updatable = false)
@@ -43,5 +47,4 @@ public class User {
     @LastModifiedDate
     @Column(nullable = false)
     private LocalDateTime updateAt;
-
 }
--- src/main/java/com/people/findclothes/domain/constant/UserRole.java
+++ src/main/java/com/people/findclothes/domain/constant/UserRole.java
@@ -1,11 +1,11 @@
 package com.people.findclothes.domain.constant;
 
-public enum UserStatus {
-    BASIC("일반"), SOCIAL("소셜"), ADMIN("관리자");
+public enum UserRole {
+    MEMBER("일반 회원"), ADMIN("관리자");
 
     private final String displayValue;
 
-    UserStatus(String displayValue) {
+    UserRole(String displayValue) {
         this.displayValue = displayValue;
     }
 

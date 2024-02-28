f82cecf5ab106527698b50b2be01428ef1100f69
chy0503 <202015006@kyonggi.ac.kr>
2024-02-28 21:37:05 +0900

    test : 유저 저장, 조회 추가

--- src/test/java/com/people/findclothes/repository/UserRepositoryTest.java
+++ src/test/java/com/people/findclothes/repository/UserRepositoryTest.java
@@ -1,31 +1,62 @@
 package com.people.findclothes.repository;
 
 import com.people.findclothes.domain.User;
-import com.people.findclothes.domain.constant.UserStatus;
+import com.people.findclothes.domain.constant.UserRole;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-@SpringBootTest
-@Transactional
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
 
+    @DisplayName("유저 저장")
     @Test
-    public void insertTest() {
+    public void saveUser() {
+        // given
         User user = User.builder()
                 .id("userId")
                 .password("1234")
                 .email("email@google.com")
                 .nickname("nickname")
                 .name("신짱구")
-                .userStatus(UserStatus.BASIC)
+                .userRole(UserRole.MEMBER)
                 .build();
 
-        userRepository.save(user);
+        // when
+        User savedUser = userRepository.save(user);
+
+        // then
+        assertThat(savedUser.getId()).isEqualTo(user.getId());
+        assertThat(savedUser.getUserRole()).isEqualTo(user.getUserRole());
     }
 
+    @DisplayName("유저 조회")
+    @Test
+    public void findUser() {
+        // given
+        User user = User.builder()
+                .id("userId")
+                .password("1234")
+                .email("email@google.com")
+                .nickname("nickname")
+                .name("신짱구")
+                .userRole(UserRole.MEMBER)
+                .build();
+
+        // when
+        User savedUser = userRepository.save(user);
+        User findUser = userRepository.findById(savedUser.getId()).get();
+
+        // then
+        assertThat(savedUser).isSameAs(findUser);
+        assertThat(findUser.getCreateAt()).isEqualTo(savedUser.getCreateAt());
+    }
 }

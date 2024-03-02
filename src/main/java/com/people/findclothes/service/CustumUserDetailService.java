3b617812efd0c4afb0b5973c9867e4f862c95422
chy0503 <202015006@kyonggi.ac.kr>
2024-03-03 04:34:29 +0900

    #4 - Spring Security User 구현

---
+++ src/main/java/com/people/findclothes/service/CustumUserDetailService.java
@@ -0,0 +1,29 @@
+package com.people.findclothes.service;
+
+import com.people.findclothes.domain.User;
+import com.people.findclothes.dto.security.CustomUserDetails;
+import com.people.findclothes.exception.UserNotFoundException;
+import com.people.findclothes.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class CustumUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String id) throws UsernameNotFoundException {
+        User user = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("해당 ID의 유저가 존재하지 않아 로그인 실패하였습니다."));
+
+        return CustomUserDetails.builder()
+                .user(user)
+                .build();
+    }
+}

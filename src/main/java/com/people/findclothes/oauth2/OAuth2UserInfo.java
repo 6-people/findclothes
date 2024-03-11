dcde44f5d79d648fa94e8c24b2f3a118708110a8
chy0503 <202015006@kyonggi.ac.kr>
2024-03-12 04:32:13 +0900

    #6 - oauth2 client용 파일 삭제

--- src/main/java/com/people/findclothes/oauth2/OAuth2UserInfo.java
+++
@@ -1,8 +0,0 @@
-package com.people.findclothes.oauth2;
-
-public interface OAuth2UserInfo {
-    String getProviderId();
-    String getProvider();
-    String getEmail();
-    String getNickname();
-}

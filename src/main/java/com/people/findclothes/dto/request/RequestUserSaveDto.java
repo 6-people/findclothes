b19b6aa53d84948dcad198d3f9747db184b6e8b5
chy0503 <202015006@kyonggi.ac.kr>
2024-03-12 04:36:18 +0900

    #6 - setter 대신 builder로 변경

--- src/main/java/com/people/findclothes/dto/request/RequestUserSaveDto.java
+++ src/main/java/com/people/findclothes/dto/request/RequestUserSaveDto.java
@@ -1,13 +1,17 @@
 package com.people.findclothes.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.ToString;
 
 @Getter
-@Setter
+@Builder
+@ToString
+@Schema(description = "유저 일반 회원가입 요청 DTO")
 public class RequestUserSaveDto {
     @Size(min = 3, max = 25)
     @NotEmpty(message = "사용자ID는 필수항목입니다.")

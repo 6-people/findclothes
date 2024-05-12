926b1c00800b2eaa715e06023d66a3ff76aab1af
chy0503 <202015006@kyonggi.ac.kr>
2024-05-12 14:44:15 +0900

    config : cors 설정

---
+++ src/main/java/com/people/findclothes/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package com.people.findclothes.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 해당 설정을 적용할 API 범위 : 전체
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+
+}
